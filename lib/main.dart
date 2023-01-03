dab1859c2b40d31f98cb9456d7b90fd89e54f418
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2023-01-03 22:36:23 +0900

    btn

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class App extends StatelessWidget {
             body: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 40),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   SizedBox(height: 80),
                   Row(
@@ -44,6 +45,51 @@ class App extends StatelessWidget {
                               )),
                         ],
                       )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 120,
+                  ),
+                  Text(
+                    'Total Balance',
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.8),
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    '\$5 194 482',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 44,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(45),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 50,
+                            vertical: 20,
+                          ),
+                          child: Text("Transfer",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 22,
+                              )),
+                        ),
+                      ),
                     ],
                   )
                 ],
