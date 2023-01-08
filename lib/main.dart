c345fec1b03f9acd77e0828ada2300d7bd416312
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2023-01-08 17:01:09 +0900

    icons and transform

--- lib/main.dart
+++ lib/main.dart
@@ -115,6 +115,7 @@ class App extends StatelessWidget {
                 height: 20,
               ),
               Container(
+                clipBehavior: Clip.hardEdge,
                 decoration: const BoxDecoration(
                   color: Color(0xFF1F2123),
                   borderRadius: BorderRadius.all(
@@ -126,6 +127,7 @@ class App extends StatelessWidget {
                     30,
                   ),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -163,6 +165,17 @@ class App extends StatelessWidget {
                             ],
                           ),
                         ],
+                      ),
+                      Transform.scale(
+                        scale: 2.2,
+                        child: Transform.translate(
+                          offset: const Offset(-2, 15),
+                          child: const Icon(
+                            Icons.euro_rounded,
+                            color: Colors.white,
+                            size: 60,
+                          ),
+                        ),
                       ),
                     ],
                   ),
