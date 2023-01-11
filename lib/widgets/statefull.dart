42bc3550a6e0f8ac014faa229df05f5ad21ad6b7
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2023-01-11 19:28:25 +0900

    widget lifecycle

--- lib/widgets/statefull.dart
+++ lib/widgets/statefull.dart
@@ -19,6 +19,18 @@ class _StateFullExampleState extends State<StateFullExample> {
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
